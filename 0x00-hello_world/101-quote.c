ad57438e45d4af9dfdc519a842dcf81a27a3c8d7
DevWambugu <venusdata100@gmail.com>
2023-03-16 11:58:38 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,8 @@
 int main(void)
 {
 	const char message[] =
-	       
-    	    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+ 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 
 	ssize_t len = sizeof(message) - 1;
