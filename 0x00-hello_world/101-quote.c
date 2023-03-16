01a4a4b08cd03f4fd029a008ec8b87e3e61270f0
DevWambugu <venusdata100@gmail.com>
2023-03-16 11:14:20 -0700

    betty's documentation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,11 @@
  */
 int main(void)
 {
-	const char message[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	ssize_t len = sizeof(message) - 1;
-	write(2,message , len);
+
+	write(2,message,len);
 
 	return (1);
 }
