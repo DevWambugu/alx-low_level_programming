ab21e6e7d57fc19f2aaea8f12e94d3abb7602643
DevWambugu <venusdata100@gmail.com>
2023-03-17 11:10:45 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - print all single digit numbers of base 10 starting from 0
+*	use putchar
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
