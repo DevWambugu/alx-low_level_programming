0868006d733146fcf12752d8e14b4cbff5bfbb65
DevWambugu <venusdata100@gmail.com>
2023-03-19 04:25:06 -0700

    Print combinations of digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+*main - print all possible different combinations of two digits
+*
+*Return: Always 0
+*/
+int main(void)
+{
+	int k;
+	int j;
+
+	for (j = 0; j < 9; j++)
+		for (k = (j + 1); k < 10; k++)
+
+		putchar(j + '0');
+		putchar(k + '0');
+		if (j != 9)
+			putchar(',');
+			putchar(' ');
+	return (0);
+}
