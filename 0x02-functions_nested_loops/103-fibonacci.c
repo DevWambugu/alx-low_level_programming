a2c3dee18ce447ea82bfaa04ee3487e9e1cf5a99
DevWambugu <venusdata100@gmail.com>
2023-03-22 16:20:21 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - print the sum of the even-valued terms, followed by a new line.
+*	in the fibonacci series
+*
+*Return: always(0)
+*/
+int main(void)
+{
+	unsigned long j = 1;
+	unsigned long fib;
+	unsigned long k =  2;
+	unsigned long sum = 3;
+
+	printf("%lu, %lu", j, k);
+	while (fib <= 4000000)
+	{
+		fib = (j + k);
+		printf("%lu", fib);
+		j = k;
+		k = fib;
+		if (fib % 2 == 0)
+			sum = (sum + fib);
+	}
+return (0);
+}
---
+++ 0x02-functions_nested_loops/REEADME.md
@@ -0,0 +1,1 @@
+15. Even Liber Abbaci
