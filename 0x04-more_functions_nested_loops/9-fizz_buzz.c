5743980f89d6dc5c3f29cace864a9bcdb4f44b3c
DevWambugu <venusdata100@gmail.com>
2023-03-23 14:44:26 -0700

    add space after fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,7 +19,7 @@ int main(void)
 	else if ((i % 5) == 0 && (i % 15) != 0)
 		printf("Buzz ");
 	else if ((i % 15) == 0)
-		printf("Fizz Buzz");
+		printf("Fizz Buzz ");
 	else
 	printf("%d ", i);
 	}
