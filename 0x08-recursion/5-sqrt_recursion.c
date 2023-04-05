6dce05b102823cb73ab34e4ed8410dc735075c70
DevWambugu <venusdata100@gmail.com>
2023-04-05 14:06:25 -0700

    find the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+*_sqrt_recursion -  returns the natural square root of a number
+*@n: integer
+*
+*Return: returns the natural square root of a number
+*/
+int _sqrt_recursion(int n)
+{
+	double E = (n / 2);
+	int SE;
+	int rtn;
+	int N = n;
+
+	if (n < 0)
+	{
+		rtn = -1;
+	}
+	else if (n == 1)
+	{
+		rtn = 1;
+	}
+	else if (n == 0)
+	{
+		rtn = 0;
+	}
+	else
+	{
+		SE = (E + (N / E)) / 2;
+		 _sqrt_recursion(SE);
+		E = SE;
+		N = n;
+		if ((SE - E) <= 0.01)
+		{
+			rtn = SE;
+		}
+	}
+	return (rtn);
+}
