6f06315eebbdd17c4edd30ed0dffbc504dbfdc38
DevWambugu <venusdata100@gmail.com>
2023-04-17 04:00:17 -0700

    create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
-*dog_t - creates a new dog
+*new_dog - creates a new dog
 *@age: age of dog
 *@owner: owner of the dog
 *@name: name of new dog
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int name_length = 0;
 	int i = 0;
 	int j = 0;
-	int owner_length = 0;
+	int owner_len = 0;
 	dog_t *new_dog_pointer;
 
 	new_dog_pointer = malloc(sizeof(dog_t));
@@ -23,30 +23,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		return (NULL);
 	}
-	for(i = 0; name[i];i++)
+	for (i = 0; name[i]; i++)
 	{
 		name_length++;
 	}
 	for (j = 0; owner[j]; j++)
 	{
-		owner_length++;
+		owner_len++;
 	}
 	new_dog_pointer->name = malloc(name_length * sizeof(new_dog_pointer->name));
 	if (new_dog_pointer == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	for (i = 0; i < name_length; i++)
 	{
 		(new_dog_pointer->name)[i] = name[i];
 	}
 	new_dog_pointer->age = age;
-	new_dog_pointer->owner = malloc(owner_length * sizeof(new_dog_pointer->owner));
+	new_dog_pointer->owner = malloc(owner_len * sizeof(new_dog_pointer->owner));
 	if (new_dog_pointer == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
-	for (i = 0; i < owner_length; i++)
+	for (i = 0; i < owner_len; i++)
 	{
 		new_dog_pointer->owner[i] = owner[i];
 	}
