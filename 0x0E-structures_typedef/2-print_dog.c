ca2373217d43d84a67d664ac43c90bdd952322a5
DevWambugu <venusdata100@gmail.com>
2023-04-18 04:28:04 -0700

    trial 4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,13 +20,13 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", (*d).name);
 		}
-		if (d->age)
+		if (d->age == -1.0)
 		{
-			printf("Age: %f\n", (*d).age);
+			printf("Age: 0.000000");
 		}
 		else
 		{
-			printf("Age: 0.000000");
+			printf("Age: %f\n", d->age);
 		}
 		if (d->owner == NULL)
 		{
