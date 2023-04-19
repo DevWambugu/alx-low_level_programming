dab9a2c22612a15eb967f22d1f6bf3ed22e78c62
DevWambugu <venusdata100@gmail.com>
2023-04-19 14:35:42 -0700

    betty compliant

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,19 +4,20 @@
 *get_op_func - selects the correct function
 *@s: the operator passed to the function
 *
-*Returns  pointer to the function to perform the operation
+*Return: pointer to the function to perform the operation
 */
 int (*get_op_func(char *s))(int, int)
 {
 		op_t ops[] = {
-	        {"+", op_add},
-        	{"-", op_sub},
-        	{"*", op_mul},
-        	{"/", op_div},
-        	{"%", op_mod},
-	        {NULL, NULL}
-	    };
-	    int i = 0;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
 	while (ops[i].op)
 	{
 		if (*s == *ops[i].op)
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -2,24 +2,30 @@
 
 /**
 *op_add - adds to integers
+*@a: integer a
+*@b: integer b
 *
 *Return: return sum of a and b
 */
 int op_add(int a, int b)
 {
-	return (a +b);
+	return (a + b);
 }
 /**
 *op_sub - subtracts b from a
+*@a: integer a
+*@b: integer b
 *
 *Return: returns the difference
 */
 int op_sub(int a, int b)
 {
-	return (a-b);
+	return (a - b);
 }
 /**
 *op_mul - multiplies a and b
+*@a: integer a
+*@b: integer b
 *
 *Return: returns the product
 */
@@ -29,15 +35,19 @@ int op_mul(int a, int b)
 }
 /**
 *op_div - returns the division
+*@a: integer a
+*@b: integer b
 *
 *Return: returns the division
 */
 int op_div(int a, int b)
 {
- 	return (a / b);
+	return (a / b);
 }
 /**
 *op_mod - gets te remainder
+*@a: integer a
+*@b: integer b
 *
 *Return: returns the remainder of the division
 */
