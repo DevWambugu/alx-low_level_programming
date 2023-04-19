8a6ff93e7f532975f7be3da9785e2492948bd643
DevWambugu <venusdata100@gmail.com>
2023-04-19 14:03:37 -0700

    betty main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,6 +2,8 @@
 
 /**
 *main - returns an integer valuebased on operator
+*@argc: argument count
+*@argv: argument vector
 *
 *Return: an integer value
 */
@@ -22,13 +24,15 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(100);
 	}
-	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 && strcmp(argv[2], "%") != 0
-		&& strcmp(argv[2], "*")!=0 && strcmp(argv[2], "/") != 0) 
+	if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 &&
+		strcmp(argv[2], "%") != 0 && strcmp(argv[2], "*") != 0
+		&& strcmp(argv[2], "/") != 0)
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	f = get_op_func(argv[2]);
 	result = f(num1, num2);
-	return (result);
+	printf("%d\n", result);
+	return (0);
 }
