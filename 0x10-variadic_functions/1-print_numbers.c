24d0291e18c913a78e4badfcaeec08975e8aa8b4
DevWambugu <venusdata100@gmail.com>
2023-04-20 03:34:21 -0700

    change separator condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,18 +14,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (*separator)
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(ap, n);
-		for (i = 0; i < n; i++)
+		printf("%d", va_arg(ap, int));
+		if (i < (n - 1) && separator != NULL)
 		{
-			printf("%d", va_arg(ap, int));
-			if (i < (n - 1))
-			{
-				printf("%s", separator);
-			}
+			printf("%s", separator);
 		}
-		va_end(ap);
-		printf("\n");
 	}
+	va_end(ap);
+	printf("\n");
 }
