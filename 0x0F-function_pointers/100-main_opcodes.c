848909b16b1fba0dbd1d5d61556ca7da4eebbb3b
DevWambugu <venusdata100@gmail.com>
2023-04-21 12:09:32 -0700

    edit code

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,23 +2,14 @@
 #include <stdlib.h>
 
 /**
-*print_opcodes - prints out opcodes.number printed
-*	depends on the number of bytes specified
-*@strt: start of main function optcodes
-*@length: number of optcodes
-*
-*Return: returns nothing. This nis a void function
-*/
-void print_opcodes(unsigned char *strt, int length);
-/**
 *main - prints out the opcodes. number of opcodes
 *	printed depends on the number of bytes specified
 *@argc: argument count
 *@argv: argument vector
 *
-*Return: Always return 1 or 2
+*Return: Always return 1 or 2 or 0
 */
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
@@ -26,32 +17,22 @@ int main(int argc, char **argv[])
 		exit(1);
 	}
 
-	int no_of_bytes = atoi((const char)*argv[1]);
+	int no_of_bytes = atoi(argv[1]);
 
 	if (no_of_bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	unsigned char *strt = (unsigned char)*main;
-
-	print_opcodes(strt, no_of_bytes);
-	return (0);
-}
-/**
-*print_opcodes - prints the opcodes
-*@strt: start of the opcodes
-*@length: number of opcodes to be printed
-*
-*Return: return nothing
-*/
-void print_opcodes(unsigned char *strt, int length)
-{
+	char *strt = (char *)main;
 	int i = 0;
 
-	for (i = 0; i < length; i++)
+	for (i = 0; i < no_of_bytes; i++)
 	{
-		printf("%02x", *(strt + i));
+		printf("%02x", strt[i] & 0xFF);
+		if (i != no_of_bytes - 1)
+			printf(" ");
 	}
 	printf("\n");
+	return (0);
 }
