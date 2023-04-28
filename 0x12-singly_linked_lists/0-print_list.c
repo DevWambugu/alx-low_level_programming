e709b3e6c4109086fc0d84497b126460295ed8bb
DevWambugu <venusdata100@gmail.com>
2023-04-28 11:04:59 -0700

    remove null condition

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,14 +15,7 @@ size_t print_list(const list_t *h)
 
 	while (h != 0)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%d] %s \n", h->len, h->str);
-		}
+		printf("[%d] %s \n", h->len, h->str);
 		i++;
 		h = h->next;
 	}
