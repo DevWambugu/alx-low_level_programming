25a5bc282c6f15ccc12774f077c6716e63a2cd10
DevWambugu <venusdata100@gmail.com>
2023-04-28 11:32:58 -0700

    include condition in the while loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,13 +13,16 @@ size_t print_list(const list_t *h)
 {
 	size_t j = 0;
 
-	if (h == NULL)
-	{
-		printf("[0] (nil)");
-	}
 	while (h != NULL)
 	{
-		printf("[%d]%s\n", h->len, h->str);
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d]%s\n", h->len, h->str);
+		}
 		h = h->next;
 		j++;
 	}
