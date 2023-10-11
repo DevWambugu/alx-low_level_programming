60cfa91a746900846c93cb53927b5b99e6626293
DevWambugu <venusdata100@gmail.com>
2023-10-11 18:58:41 +0000

    skip list linked list

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "search_algos.h"
+/**
+  *linear_skip - searches a value in a linked list
+  *@list: the linked list containing the value
+  *@value: The value to search for
+  *
+  *Return: Returns NULL or a poiter to the first node
+  */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *current = list;
+	skiplist_t *express_lane = NULL;
+
+	if (list == NULL)
+	{
+		return (NULL);
+	}
+
+	while (current && current->n < value)
+	{
+		express_lane = current;
+		if (current->express != NULL)
+		{
+			current = current->express;
+			printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+		}
+		else
+		{
+			while (current->next != NULL && current->next->n < value)
+			{
+				current = current->next;
+				printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+			}
+		}
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", express_lane->index, current->index);
+	while (express_lane->index <= current->index)
+	{
+		printf("Value checked at index [%lu] = [%d]\n", express_lane->index, express_lane->n);
+		if (express_lane->n == value)
+		{
+			return (express_lane);
+		}
+		express_lane = express_lane->next;
+	}
+	return (NULL);
+}
